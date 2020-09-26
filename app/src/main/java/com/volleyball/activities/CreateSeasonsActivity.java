0842bc82fbf4255232bb2557050bed580b5f12c8
sankeerth1020 <erravellisankeerth@gmail.com>
2020-09-26 14:58:38 -0400

    Create Season image upload java code

--- app/src/main/java/com/volleyball/activities/CreateSeasonsActivity.java
+++ app/src/main/java/com/volleyball/activities/CreateSeasonsActivity.java
@@ -1,21 +1,52 @@
 package com.volleyball.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.volleyball.R;
+import com.volleyball.api.ApiService;
+import com.volleyball.models.ResponseData;
 
-public class CreateSeasonsActivity extends AppCompatActivity {
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import pub.devrel.easypermissions.EasyPermissions;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class CreateSeasonsActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+
+    private static final String TAG = CreateSeasonsActivity.class.getSimpleName();
     private static final int REQUEST_GALLERY_CODE = 200;
+    private static final int READ_REQUEST_CODE = 300;
+    private static final String SERVER_PATH = "http://cegepvolleyleagues.com/";
+    private Uri uri;
     EditText et_season_name;
     Button btn_season_img,btn_Submit;
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +71,89 @@ public class CreateSeasonsActivity extends AppCompatActivity {
         });
 
         btn_Submit = (Button)findViewById(R.id.btn_Submit);
+        btn_Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadImageToServer();
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, CreateSeasonsActivity.this);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_GALLERY_CODE && resultCode == Activity.RESULT_OK){
+            uri = data.getData();
+            if(EasyPermissions.hasPermissions(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                String filePath = getRealPathFromURIPath(uri, CreateSeasonsActivity.this);
+                file = new File(filePath);
+
+            }else{
+                EasyPermissions.requestPermissions(this, getString(R.string.read_file), READ_REQUEST_CODE, Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+        }
+    }
+    private String getRealPathFromURIPath(Uri contentURI, Activity activity) {
+        Cursor cursor = activity.getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) {
+            return contentURI.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            return cursor.getString(idx);
+        }
+    }
+    File file;
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+        if(uri != null){
+            String filePath = getRealPathFromURIPath(uri, CreateSeasonsActivity.this);
+            file = new File(filePath);
+            // uploadImageToServer();
+        }
+    }
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        Log.d(TAG, "Permission has been denied");
+
+    }
+
+    private void uploadImageToServer(){
+        pd=new ProgressDialog(CreateSeasonsActivity.this);
+        pd.setTitle("Loading");
+        pd.show();
+        Map<String, String> map = new HashMap<>();
+        map.put("s_name",et_season_name.getText().toString());
+
+
+        RequestBody mFile = RequestBody.create(MediaType.parse("image/*"), file);
+        MultipartBody.Part fileToUpload = MultipartBody.Part.createFormData("file", file.getName(), mFile);
+        RequestBody filename = RequestBody.create(MediaType.parse("text/plain"), file.getName());
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(SERVER_PATH)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        ApiService uploadImage = retrofit.create(ApiService.class);
+        Call<ResponseData> fileUpload = uploadImage.addseasons(fileToUpload, map);
+        fileUpload.enqueue(new Callback<ResponseData>() {
+            @Override
+            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                pd.dismiss();
+                Toast.makeText(CreateSeasonsActivity.this, response.body().message, Toast.LENGTH_LONG).show();
+                //finish();
+            }
+            @Override
+            public void onFailure(Call<ResponseData> call, Throwable t) {
+                pd.dismiss();
+                Toast.makeText(CreateSeasonsActivity.this, "Error :"+t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/volleyball/api/ApiService.java
+++ app/src/main/java/com/volleyball/api/ApiService.java
@@ -3,8 +3,16 @@ package com.volleyball.api;
 
 
 import com.volleyball.models.ResponseData;
+
+import java.util.Map;
+
+import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.PartMap;
 import retrofit2.http.Query;
 
 public interface ApiService {
@@ -22,5 +30,11 @@ public interface ApiService {
             @Query("password") String password
     );
 
+    @Multipart
+    @POST("Volleyball/addseasons.php?")
+    Call<ResponseData> addseasons(
+            @Part MultipartBody.Part file,
+            @PartMap Map<String, String> partMap
+    );
 
 }

57c9b5b38970174e9cc3118a6b75baa3be821f70
ajaykanna225 <kastalaajay@gmail.com>
2020-09-26 18:19:12 -0400

    modified home fragment

--- app/src/main/java/com/volleyball/fragments/HomeFragment.java
+++ app/src/main/java/com/volleyball/fragments/HomeFragment.java
@@ -1,11 +1,31 @@
 package com.volleyball.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-public class HomeFragment extends Fragment {
+import com.volleyball.R;
 
+public class HomeFragment extends Fragment {
+    View view;
     public static HomeFragment homeFragment() {
         HomeFragment fragment = new HomeFragment();
         return fragment;
+    }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Home");
+
+
+        return view;
     }
 }

03cd162b846d196935410338d97fbf63198ca007
dhanikt <dhaniktunuguntla@gmail.com>
2020-10-08 13:54:27 -0400

    Create Team xml file

--- app/src/main/java/com/volleyball/activities/CreateTeamActivity.java
+++ app/src/main/java/com/volleyball/activities/CreateTeamActivity.java
@@ -11,6 +11,6 @@ public class CreateTeamActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_team2);
+        setContentView(R.layout.activity_create_team);
     }
 }

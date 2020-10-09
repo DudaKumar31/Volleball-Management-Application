70043306233f1a561b0c820b464ca5d90b3b901c
sankeerth1020 <erravellisankeerth@gmail.com>
2020-10-09 17:14:12 -0400

    GetAllSeasons for guest user added

--- app/src/main/java/com/volleyball/activities/GetAllSeasonsActivity.java
+++ app/src/main/java/com/volleyball/activities/GetAllSeasonsActivity.java
@@ -1,16 +1,85 @@
 package com.volleyball.activities;
 
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.volleyball.R;
+import com.volleyball.adapters.GetAllSeasonsAdapter;
+import com.volleyball.api.ApiService;
+import com.volleyball.api.RetroClient;
+import com.volleyball.models.GetAllSeasonsPojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class GetAllSeasonsActivity extends AppCompatActivity {
-
+    ListView list_view;
+    List<GetAllSeasonsPojo> allSeasons;
+    View view;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_all_seasons);
+
+        getSupportActionBar().setTitle("All Seasons");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        list_view=(ListView)findViewById(R.id.list_view);
+        allSeasons= new ArrayList<>();
+        serverData();
+
+
+    }
+    ProgressDialog progressDialog;
+    public void serverData(){
+        progressDialog = new ProgressDialog(GetAllSeasonsActivity.this);
+        progressDialog.setMessage("Loading....");
+        progressDialog.show();
+
+        ApiService service = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<List<GetAllSeasonsPojo>> call = service.getseasons();
+        call.enqueue(new Callback<List<GetAllSeasonsPojo>>() {
+            @Override
+            public void onResponse(Call<List<GetAllSeasonsPojo>> call, Response<List<GetAllSeasonsPojo>> response) {
+                //Toast.makeText(GetAllSeasonsActivity.this,"ddddd   "+response.body().size(),Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+                if(response.body()==null){
+                    Toast.makeText(GetAllSeasonsActivity.this,"No data found",Toast.LENGTH_SHORT).show();
+                }else {
+                    allSeasons = response.body();
+                    list_view.setAdapter(new GetAllSeasonsAdapter(allSeasons, GetAllSeasonsActivity.this));
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<GetAllSeasonsPojo>> call, Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(GetAllSeasonsActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
---
+++ app/src/main/java/com/volleyball/adapters/GetAllSeasonsAdapter.java
@@ -0,0 +1,70 @@
+package com.volleyball.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.volleyball.R;
+import com.volleyball.models.GetAllSeasonsPojo;
+
+import java.util.List;
+
+public class GetAllSeasonsAdapter extends BaseAdapter {
+    List<GetAllSeasonsPojo> getAllSeasonsPojo;
+    Context cnt;
+    public GetAllSeasonsAdapter(List<GetAllSeasonsPojo> ar, Context cnt)
+    {
+        this.getAllSeasonsPojo=ar;
+        this.cnt=cnt;
+    }
+    @Override
+    public int getCount() {
+        return getAllSeasonsPojo.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return i;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(final int pos, View view, ViewGroup viewGroup)
+    {
+        LayoutInflater obj1 = (LayoutInflater)cnt.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View obj2=obj1.inflate(R.layout.list_current_seasons,null);
+
+
+        ImageView iv_image_view=(ImageView)obj2.findViewById(R.id.iv_image_view);
+        Glide.with(cnt).load(getAllSeasonsPojo.get(pos).getS_logo()).into(iv_image_view);
+
+
+
+        TextView tv_text=(TextView)obj2.findViewById(R.id.tv_text);
+        tv_text.setText(getAllSeasonsPojo.get(pos).getS_name());
+
+        iv_image_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent=new Intent(cnt, GetAllTeamsActivity.class);
+//                cnt.startActivity(intent);
+            }
+        });
+
+
+        return obj2;
+    }
+
+
+
+}

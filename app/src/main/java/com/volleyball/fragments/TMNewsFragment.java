1534919f54d685f295aaa8f80a6885b66cc3d598
dhanikt <dhaniktunuguntla@gmail.com>
2020-10-21 17:01:38 -0400

    Team Manager  News fragment added

--- app/src/main/java/com/volleyball/fragments/TMNewsFragment.java
+++ app/src/main/java/com/volleyball/fragments/TMNewsFragment.java
@@ -1,5 +1,6 @@
 package com.volleyball.fragments;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -18,14 +19,23 @@ import androidx.fragment.app.Fragment;
 import com.volleyball.R;
 import com.volleyball.activities.LoginActivity;
 import com.volleyball.activities.TMEditProfileActivity;
+import com.volleyball.adapters.AllNewsAdapter;
+import com.volleyball.api.ApiService;
+import com.volleyball.api.RetroClient;
+import com.volleyball.models.GetAllNewsPojo;
 
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class TMNewsFragment extends Fragment {
     ListView list_view;
-
+    ProgressDialog progressDialog;
+    List<GetAllNewsPojo> al;
     View view;
 
 
@@ -39,21 +49,34 @@ public class TMNewsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         view = inflater.inflate(R.layout.fragment_t_m_news, container, false);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("News");
+        list_view=(ListView)view.findViewById(R.id.list_view);
 
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("News");
+        loadAllNews();
 
         return view;
     }
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.tm_main_menu, menu);
-        super.onCreateOptionsMenu(menu, inflater);
+    ProgressDialog pd;
+    private void loadAllNews(){
+        pd = new ProgressDialog(getActivity());
+        pd.setTitle("Please wait,Data is being loaded.");
+        pd.show();
+        ApiService api = RetroClient.getApiService();
+        Call<List<GetAllNewsPojo>> call = api.getnews();
+        call.enqueue(new Callback<List<GetAllNewsPojo>>() {
+            @Override
+            public void onResponse(Call<List<GetAllNewsPojo>> call, Response<List<GetAllNewsPojo>> response) {
+                pd.dismiss();
+                if (response.isSuccessful()) {
+                    List<GetAllNewsPojo> teams=response.body();
+                    list_view.setAdapter(new AllNewsAdapter(teams,getActivity()));
+                }
+            }
+            @Override
+            public void onFailure(Call<List<GetAllNewsPojo>> call, Throwable t) {
+                pd.dismiss();
+            }
+        });
     }
 
     @Override

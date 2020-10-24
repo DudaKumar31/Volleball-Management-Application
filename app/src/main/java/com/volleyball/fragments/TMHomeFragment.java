23104b69b6d00e9f15b3e292cd59c3f25eee60d9
dhanikt <dhaniktunuguntla@gmail.com>
2020-10-23 22:09:22 -0400

    Team Manager home fragment added

--- app/src/main/java/com/volleyball/fragments/TMHomeFragment.java
+++ app/src/main/java/com/volleyball/fragments/TMHomeFragment.java
@@ -1,7 +1,9 @@
 package com.volleyball.fragments;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -9,7 +11,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,6 +25,8 @@ import androidx.fragment.app.Fragment;
 import com.volleyball.R;
 import com.volleyball.activities.LoginActivity;
 import com.volleyball.activities.TMEditProfileActivity;
+import com.volleyball.activities.TeamManagerAddPlayerActivity;
+import com.volleyball.activities.Utils;
 
 
 import java.util.ArrayList;
@@ -27,6 +35,12 @@ import java.util.List;
 public class TMHomeFragment extends Fragment {
     ProgressDialog progressDialog;
     View view;
+    String session;
+    Button btn_add_player;
+    TextView tv_team_name;
+    ImageView team_logo;
+    SharedPreferences sharedPreferences;
+    ListView list_view;
 
     public static TMHomeFragment tmHomeFragment() {
         TMHomeFragment fragment = new TMHomeFragment();
@@ -39,6 +53,36 @@ public class TMHomeFragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         view = inflater.inflate(R.layout.fragment_t_m_home, container, false);
         ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Home");
+
+        list_view = (ListView) view.findViewById(R.id.list_view);
+        tv_team_name=(TextView)view.findViewById(R.id.tv_team_name);
+        team_logo=(ImageView)view.findViewById(R.id.team_logo);
+
+
+        sharedPreferences = getActivity().getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+        session = sharedPreferences.getString("team_id", "def-val");
+
+        btn_add_player=(Button)view.findViewById(R.id.btn_add_player);
+        btn_add_player.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(no_of_players<=5) {
+                    Intent intent=new Intent(getActivity(), TeamManagerAddPlayerActivity.class);
+                    intent.putExtra("teamid",session);
+                    intent.putExtra("teamname",tv_team_name.getText().toString());
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getActivity(),"Team is full.",Toast.LENGTH_SHORT).show();
+                }
+
+
+
+
+                //finish();
+            }
+        });
+
         return view;
 
     }

2bea09f73f89c0a45342324a863d6b7e77732bd3
DudaKumar31 <kumarduda31@gmail.com>
2020-10-23 23:12:52 -0400

    NewsDetailsActivity Supported Files Added

--- app/src/main/java/com/volleyball/activities/NewsDetailsActivity.java
+++ app/src/main/java/com/volleyball/activities/NewsDetailsActivity.java
@@ -3,14 +3,42 @@ package com.volleyball.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.volleyball.R;
 
 public class NewsDetailsActivity extends AppCompatActivity {
+    ImageView iv_image_view;
+    TextView tv_title,tv_descriptiop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_details);
+
+        getSupportActionBar().setTitle("News Details");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        iv_image_view=(ImageView)findViewById(R.id.iv_image_view);
+        tv_title=(TextView)findViewById(R.id.tv_title);
+        tv_descriptiop=(TextView)findViewById(R.id.tv_descriptiop);
+
+        Glide.with(NewsDetailsActivity.this).load(getIntent().getStringExtra("image")).into(iv_image_view);
+        tv_title.setText(getIntent().getStringExtra("title"));
+        tv_descriptiop.setText(getIntent().getStringExtra("description"));
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }

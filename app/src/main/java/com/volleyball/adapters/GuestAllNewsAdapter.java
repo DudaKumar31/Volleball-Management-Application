65de4fff3057404dd8b14fa44f27087ad889e64b
harikareddy2207 <harikareddy22g@gmail.com>
2020-10-23 23:52:31 -0400

    LMNewsFragment added

---
+++ app/src/main/java/com/volleyball/adapters/GuestAllNewsAdapter.java
@@ -0,0 +1,143 @@
+package com.volleyball.adapters;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.volleyball.R;
+import com.volleyball.activities.AllSeasonsActivity;
+import com.volleyball.activities.EditSeasonsActivity;
+import com.volleyball.activities.NewsDetailsActivity;
+import com.volleyball.api.ApiService;
+import com.volleyball.api.RetroClient;
+import com.volleyball.models.GetAllNewsPojo;
+import com.volleyball.models.GetAllSeasonsPojo;
+import com.volleyball.models.ResponseData;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class GuestAllNewsAdapter extends BaseAdapter {
+    ProgressDialog progressDialog;
+    List<GetAllNewsPojo> getAllNewsPojos;
+    Context cnt;
+    public GuestAllNewsAdapter(List<GetAllNewsPojo> ar, Context cnt)
+    {
+        this.getAllNewsPojos=ar;
+        this.cnt=cnt;
+    }
+    @Override
+    public int getCount() {
+        return getAllNewsPojos.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return i;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(final int pos, View view, ViewGroup viewGroup)
+    {
+        LayoutInflater obj1 = (LayoutInflater)cnt.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View obj2=obj1.inflate(R.layout.list_guest_all_news,null);
+
+        ImageView iv_image_view=(ImageView)obj2.findViewById(R.id.iv_image_view);
+        Glide.with(cnt).load(getAllNewsPojos.get(pos).getImage()).into(iv_image_view);
+
+        TextView tv_title=(TextView)obj2.findViewById(R.id.tv_title);
+        tv_title.setText(getAllNewsPojos.get(pos).getTitle());
+
+
+        iv_image_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(cnt, NewsDetailsActivity.class);
+                intent.putExtra("title",getAllNewsPojos.get(pos).getTitle());
+                intent.putExtra("image",getAllNewsPojos.get(pos).getImage());
+                intent.putExtra("id",getAllNewsPojos.get(pos).getId());
+                intent.putExtra("description",getAllNewsPojos.get(pos).getMsg());
+                cnt.startActivity(intent);
+
+
+            }
+        });
+
+
+
+
+        return obj2;
+    }
+
+    public void serverData(String id){
+        progressDialog = new ProgressDialog(cnt);
+        progressDialog.setMessage("Loading....");
+        progressDialog.show();
+        ApiService service = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<ResponseData> call = service.deleteseason(id);
+        call.enqueue(new Callback<ResponseData>() {
+            @Override
+            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                progressDialog.dismiss();
+                if(response.body()==null){
+                    Toast.makeText(cnt,"Server issue",Toast.LENGTH_SHORT).show();
+                }else {
+                    cnt.startActivity(new Intent(cnt, AllSeasonsActivity.class));
+                    Toast.makeText(cnt,"Deleted successfully",Toast.LENGTH_SHORT).show();
+                    ((Activity)cnt).finish();
+
+                }
+            }
+            @Override
+            public void onFailure(Call<ResponseData> call, Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(cnt, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void alertDioluge(final String id){
+
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(cnt);
+        builder1.setTitle("Alert !!!");
+        builder1.setMessage("Do you want to Delete the Season.");  //message we want to show the end user
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int i) {
+                        serverData(id);
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+}
--- app/src/main/java/com/volleyball/fragments/LMNewsFragment.java
+++ app/src/main/java/com/volleyball/fragments/LMNewsFragment.java
@@ -1,10 +1,106 @@
 package com.volleyball.fragments;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.volleyball.R;
+import com.volleyball.activities.LoginActivity;
+import com.volleyball.adapters.AllNewsAdapter;
+import com.volleyball.adapters.GuestAllNewsAdapter;
+import com.volleyball.api.ApiService;
+import com.volleyball.api.RetroClient;
+import com.volleyball.models.GetAllNewsPojo;
+
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class LMNewsFragment extends Fragment {
+    View view;
+    ListView list_view;
+    ProgressDialog progressDialog;
+    List<GetAllNewsPojo> al;
+
+
     public static LMNewsFragment newsFragment() {
         LMNewsFragment fragment = new LMNewsFragment();
         return fragment;
     }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        view = inflater.inflate(R.layout.fragment_l_m_news, container, false);
+        list_view=(ListView)view.findViewById(R.id.list_view);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("News");
+        loadAllNews();
+        return view;
+    }
+
+    ProgressDialog pd;
+    private void loadAllNews(){
+        pd = new ProgressDialog(getActivity());
+        pd.setTitle("Please wait,Data is being loaded.");
+        pd.show();
+        ApiService api = RetroClient.getApiService();
+        Call<List<GetAllNewsPojo>> call = api.getnews();
+        call.enqueue(new Callback<List<GetAllNewsPojo>>() {
+            @Override
+            public void onResponse(Call<List<GetAllNewsPojo>> call, Response<List<GetAllNewsPojo>> response) {
+                pd.dismiss();
+                if (response.isSuccessful()) {
+                    List<GetAllNewsPojo> teams=response.body();
+                    list_view.setAdapter(new GuestAllNewsAdapter(teams,getActivity()));
+                }
+            }
+            @Override
+            public void onFailure(Call<List<GetAllNewsPojo>> call, Throwable t) {
+                pd.dismiss();
+            }
+        });
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.main_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.logout:
+                startActivity(new Intent(getContext(), LoginActivity.class));
+                getActivity().finish();
+                return true;
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
